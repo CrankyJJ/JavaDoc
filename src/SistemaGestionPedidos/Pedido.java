cc933198576bf2270bd6c868e12953d7acb3502f
CrankyJJ <dosreisj445@gmail.com>
2024-04-02 13:12:09 +0200

    creacion del constructor Producto en la clase Producto

--- src/SistemaGestionPedidos/Pedido.java
+++ src/SistemaGestionPedidos/Pedido.java
@@ -4,4 +4,10 @@ public class Pedido {
     private String nombre;
     private String descripcion;
     private double precio;
+
+    public Producto(String nombre, String descripcion, double precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
 }
