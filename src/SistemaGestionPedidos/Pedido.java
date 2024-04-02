c5d164ff8d88f323c9d1dce9ca444c5452bffeb6
CrankyJJ <dosreisj445@gmail.com>
2024-04-02 13:19:20 +0200

    creacion de atributos de la clase Pedido

--- src/SistemaGestionPedidos/Pedido.java
+++ src/SistemaGestionPedidos/Pedido.java
@@ -1,5 +1,13 @@
 package SistemaGestionPedidos;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Pedido {
+    private int numeroPedido;
+    private ArrayList<Producto> productos;
+    private Date fechaPedido;
+    private String estado;
+    private String cliente;
+    private int mesa;
 
 }
