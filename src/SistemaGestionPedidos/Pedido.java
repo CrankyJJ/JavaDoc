532a6f1da35886a54791c6cc51029670167e650c
CrankyJJ <dosreisj445@gmail.com>
2024-04-02 13:15:52 +0200

    me he confundido de clase al poner los atributos, asi que ahora lo cambio a la clase que corresponde.

--- src/SistemaGestionPedidos/Pedido.java
+++ src/SistemaGestionPedidos/Pedido.java
@@ -1,25 +1,5 @@
 package SistemaGestionPedidos;
 
 public class Pedido {
-    private String nombre;
-    private String descripcion;
-    private double precio;
 
-    public Producto(String nombre, String descripcion, double precio) {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
 }
