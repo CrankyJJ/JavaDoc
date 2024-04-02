2806b84b0b9b83af6796f3a028232b298001d788
CrankyJJ <dosreisj445@gmail.com>
2024-04-02 13:23:38 +0200

    creacion de objetos en la clase main

--- src/SistemaGestionPedidos/Main.java
+++ src/SistemaGestionPedidos/Main.java
@@ -1,4 +1,15 @@
 package SistemaGestionPedidos;
 
+import java.util.Date;
 public class Main {
+    public static void main(String[] args) {
+
+        Producto hamburguesa = new Producto("Hamburguesa", "Una deliciosa hamburguesa con carne y vegetales", 5.99);
+        Producto refresco = new Producto("Refresco", "Refresco de cola frío", 1.50);
+        Producto helado = new Producto("Helado", "Helado de vainilla con sirope de chocolate", 2.99);
+
+
+        Pedido pedido1 = new Pedido(1, new Date(), "Juan", 2);
+
+    }
 }
