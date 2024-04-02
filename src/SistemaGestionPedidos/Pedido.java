4f1ca2dbc865ba3b9f912389d3352f17ba55d1cb
CrankyJJ <dosreisj445@gmail.com>
2024-04-02 13:20:03 +0200

    creacion del constructor Pedido en la clase Pedido

--- src/SistemaGestionPedidos/Pedido.java
+++ src/SistemaGestionPedidos/Pedido.java
@@ -10,4 +10,13 @@ public class Pedido {
     private String cliente;
     private int mesa;
 
+    public Pedido(int numeroPedido, Date fechaPedido, String cliente, int mesa) {
+        this.numeroPedido = numeroPedido;
+        this.productos = new ArrayList<>();
+        this.fechaPedido = fechaPedido;
+        this.estado = "pendiente";
+        this.cliente = cliente;
+        this.mesa = mesa;
+    }
+
 }
